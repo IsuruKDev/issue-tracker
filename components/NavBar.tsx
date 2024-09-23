698e482e40b7751e87eba821d09e6da5ba310d59
isuru kularathna <isurujr.online@gmail.com>
2024-09-23 21:47:22 +0530

    style the navbar active link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,16 +1,22 @@
+"use client"
+import classNames from 'classnames'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { BsBugFill } from 'react-icons/bs'
-import { GoBug } from 'react-icons/go'
 
 const NavBar = () => {
 
+    const currentPath = usePathname();
+
     const links = [
         {
+            id: 1,
             label: "Dashboard",
             href: "/"
         },
         {
+            id: 2,
             label: "Issues",
             href: "/issues"
         }
@@ -19,11 +25,15 @@ const NavBar = () => {
         <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
             <Link href="/"><BsBugFill /></Link>
             <ul className='flex space-x-6'>
-                {links.map(link =>
+                {links.map((link, i) =>
                     <li>
                         <Link
-                            key={link.href}
-                            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                            key={i}
+                            className={classNames({
+                                'text-zinc-900': link.href === currentPath,
+                                'text-zinc-500': link.href !== currentPath,
+                                'hover:text-zinc-800 transition-colors': true
+                            })}
                             href={link.href}>
                             {link.label}
                         </Link>
